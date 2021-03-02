d3300c770fe78983cfd0bd0d97d7742f4cbc68a7
Ann Cascarano <a.cascarano@gmail.com>
2021-03-02 14:52:41 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const button = document.querySelector("button");
+const authorSpan = document.querySelector(".author");
+const imgDiv = document.querySelector(".image-container");
+const img = document.querySelector(".img");
