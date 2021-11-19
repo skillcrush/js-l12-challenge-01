dccedbace82b654ee324303a553e7536ba9891fb
Jessica T <88570634+jmcclung3509@users.noreply.github.com>
2021-11-19 10:56:16 -0800

    Set up the API for picsum photos

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = function () {
+    const res = await fetch (
+        "https://picsum.photos/v2/list?page=2&limit=100"
+    );
+    const images = await res.json();
+    console.log(images);
+};
+getImages();
