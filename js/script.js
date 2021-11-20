192e4ad470b433777e15b4a60854a82d15f04448
Jessica T <88570634+jmcclung3509@users.noreply.github.com>
2021-11-19 21:33:22 -0800

    Created function that randomly selects an image and a function to update the image author and URL information and a click event to reveal image and author information.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,35 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = function () {
+const getImage = async function () {
     const res = await fetch (
         "https://picsum.photos/v2/list?page=2&limit=100"
     );
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 };
-getImages();
+
+
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+   console.log(randomIndex);
+
+   const randomImage = images[randomIndex];
+   //console.log(randomImage)
+   displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author =  randomImage.author;
+    const imageAddress = randomImage.download_url;
+
+    authorSpan.innerText = author;
+   img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function () {
+    getImage();
+})
